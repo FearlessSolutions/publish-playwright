f3cb5f0d4d2a8b66021768a5504b7f3594f64ac6
Tiffany Forkner <tforkner@fearless.tech>
2025-04-24 15:18:32 -0400

    adding intentionally failing test

--- tests/e2e/example.spec.ts
+++ tests/e2e/example.spec.ts
@@ -16,3 +16,9 @@ test('get started link', async ({ page }) => {
   // Expects page to have a heading with the name of Installation.
   await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
 });
+
+test('showing test failure', async ({ page }) => {
+  await page.goto("https://playwright.dev/");
+
+  await expect(page.getByText("Playwright")).not.toBeVisible();
+});
