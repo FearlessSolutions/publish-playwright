b12627e81ea64f4106c99f021440ce4e44f85097
Tiffany Forkner <tforkner@fearless.tech>
2025-04-25 09:30:35 -0400

    updating comment

--- playwright.config.ts
+++ playwright.config.ts
@@ -40,7 +40,7 @@ export default defineConfig({
     /* Base URL to use in actions like `await page.goto('/')`. */
     // baseURL: 'http://127.0.0.1:3000',
 
-    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
+    /* Collect trace for test failures. See https://playwright.dev/docs/trace-viewer */
     trace: "retain-on-failure",
     /* Save the videos for test failures. See https://playwright.dev/docs/videos */
     video: "retain-on-failure",
